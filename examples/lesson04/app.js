a6e757b1b6ae142757153e8d11817c7817cc2b00
David Bourgon Salinero <estilorama@gmail.com>
2016-12-09 12:49:19 +0100

    Added lesson04 start

---
+++ examples/lesson04/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dbourgon on 09/12/2016.
+ */
+'use strict';
+
+// IIFE
+(function(){
+
+    angular.module('myFirstApp', [])
+        .controller('MyFirstController', function() {
+
+        });
+
+})();
